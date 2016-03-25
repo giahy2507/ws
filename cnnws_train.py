c0d60d3b27c47e10cfe55ba17e573eac1a40439e
giahy2507 <giahy2507@gmail.com>
2016-03-25 08:02:34 +0000

    fix bug minibatch_i

--- cnnws_train.py
+++ cnnws_train.py
@@ -13,29 +13,23 @@ import time
 
 if __name__ == "__main__":
 
-    with open("data/vtb.pre.txt.train.np", mode="rb") as f:
+    with open("../data/vcl.pre.txt.train.pickle", mode="rb") as f:
         X_train, Y_train, X_validation, Y_validation = pickle.load(f)
 
-    X_train_size = int(X_train.shape[0]/10)
-    X_train = np.array(X_train[:X_train_size],dtype= np.int32)
-    Y_train = np.array(Y_train[:X_train_size],dtype= np.int32)
+    X_train = np.array(X_train,dtype= np.int32)
+    Y_train = np.array(Y_train,dtype= np.int32)
 
-    x_test_size = int(0.1 * X_validation.shape[0])
-
-    X_test = np.array(X_validation[:x_test_size], dtype=np.int32)
-    Y_test = np.array(Y_validation[:x_test_size], dtype=np.int32)
-
-    X_validation = np.array(X_validation[:x_test_size], dtype=np.int32)
-    Y_validation = np.array(Y_validation[:x_test_size],dtype= np.int32)
+    X_validation = np.array(X_validation, dtype=np.int32)
+    Y_validation = np.array(Y_validation, dtype= np.int32)
 
     with open("model/vocab.bin", mode="rb") as f:
         vocabulary = pickle.load(f)
 
     vocab_size = len(vocabulary.word_index.keys())
-    sys.stdout.write(str(X_train.shape)+ str(Y_train.shape) + str(X_validation.shape) + str(Y_validation.shape) + str(X_test.shape) + str(Y_test.shape))
+    sys.stdout.write(str(X_train.shape)+ str(Y_train.shape) + str(X_validation.shape) + str(Y_validation.shape) + "\n")
     sys.stdout.flush()
 
-    batch_size=100
+    batch_size=10000
     number_featuremaps = 500
     sentence_length = 5
     embsize = 500
@@ -69,16 +63,6 @@ if __name__ == "__main__":
     params = layer_projection.params + layer_conv.params + layer_hidden.params + layer_classification.params
     
     updates = adam(cost,params)
-    
-    """
-    gparams = []
-    for param in params:
-        gparam = T.grad(cost, param)
-        gparams.append(gparam)
-    updates = []
-    for param, gparam in zip(params, gparams):
-        updates.append((param, param - learning_rate* gparam))
-    """
 
     train_model = theano.function(inputs=[X,Y], outputs=[cost, err],updates=updates,on_unused_input="ignore",allow_input_downcast=True)
     valid_model = theano.function(inputs=[X,Y], outputs=[cost, err],on_unused_input="ignore",allow_input_downcast=True)
@@ -86,12 +70,11 @@ if __name__ == "__main__":
 
     counter = 0
     best_valid_err = 100
-    early_stop = 50
+    early_stop = 20
     epoch_i = 0
 
     train_rand_idxs = list(range(0,X_train.shape[0]))
     valid_rand_idxs = list(range(0,X_validation.shape[0]))
-    test_rand_idxs = list(range(0,X_test.shape[0]))
 
     while counter < early_stop:
         epoch_i +=1
@@ -101,10 +84,7 @@ if __name__ == "__main__":
 
         valid_costs = []
         valid_errs = []
-
-        test_costs = []
-        test_errs = []
-
+        
         np.random.shuffle(train_rand_idxs)
         batch_number = int(X_train.shape[0] / batch_size)
         start_train = time.clock()
@@ -117,9 +97,10 @@ if __name__ == "__main__":
             train_costs.append(train_cost)
             train_errs.append(train_err)
         end_train = time.clock()
+        
         np.random.shuffle(valid_rand_idxs)
         batch_number = int(X_validation.shape[0] / batch_size)
-        for start_idx in range(batch_number):
+        for batch_i in range(batch_number):
             mnb_X = X_validation[valid_rand_idxs[batch_i*batch_size: batch_i*batch_size + batch_size]]
             mnb_Y  = Y_validation[valid_rand_idxs[batch_i*batch_size: batch_i*batch_size + batch_size]]
             if mnb_X.shape[0] != batch_size:
@@ -127,33 +108,19 @@ if __name__ == "__main__":
             valid_cost, valid_err = valid_model(mnb_X, mnb_Y)
             valid_costs.append(valid_cost)
             valid_errs.append(valid_err)
-
-        np.random.shuffle(test_rand_idxs)
-        batch_number = int(X_test.shape[0] / batch_size)
-        for start_idx in range(batch_number):
-            mnb_X = X_test[test_rand_idxs[batch_i*batch_size: batch_i*batch_size + batch_size]]
-            mnb_Y  = Y_test[test_rand_idxs[batch_i*batch_size: batch_i*batch_size + batch_size]]
-            if mnb_X.shape[0] != batch_size:
-                continue
-            tess_cost, test_err = valid_model(mnb_X, mnb_Y)
-            test_costs.append(tess_cost)
-            test_errs.append(test_err)
-
         train_err = np.mean(np.array(train_errs))
         train_cost = np.mean(np.array(train_costs))
         val_err = np.mean(np.array(valid_errs))
         val_cost = np.mean(np.array(valid_costs))
-        test_err = np.mean(np.array(test_errs))
-        test_cost = np.mean(np.array(test_costs))
 
         if best_valid_err > val_err:
             best_valid_err = val_err
-            sys.stdout.write("Epoch "+str(epoch_i)+" Train cost: "+ str(train_cost)+ "Train err: "+ str(train_err) + "Time: " + end_train - start_train + " Validation cost: "+ str(valid_cost)+" Validation err "+ str(val_err) + " Test cost: "+ str(test_cost)+" Test err "+ str(test_err)  + ",counter "+str(counter)+ " __best__ \n")
+            sys.stdout.write("Epoch "+str(epoch_i)+" Train cost: "+ str(train_cost)+ "Train err: "+ str(train_err) + "Time: " + str(end_train - start_train) + " Validation cost: "+ str(val_cost)+" Validation err "+ str(val_err) + ",counter "+str(counter)+ " __best__ \n")
             sys.stdout.flush()
             counter = 0
             with open("model/saveweight.bin", mode="wb") as f:
                 pickle.dump(params,f)
         else:
             counter +=1
-            sys.stdout.write("Epoch "+str(epoch_i)+" Train cost: "+ str(train_cost)+ "Train err: "+ str(train_err) + "Time: " + end_train - start_train + " Validation cost: "+ str(valid_cost)+" Validation err "+ str(val_err) + " Test cost: "+ str(test_cost)+" Test err "+ str(test_err)  + ",counter "+str(counter)+ "\n")
+            sys.stdout.write("Epoch "+str(epoch_i)+" Train cost: "+ str(train_cost)+ "Train err: "+ str(train_err) + "Time: " + str(end_train - start_train) + " Validation cost: "+ str(val_cost)+" Validation err "+ str(val_err) + ",counter "+str(counter)+ "\n")
             sys.stdout.flush()
